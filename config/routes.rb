b4f34fd9d64305e19b07b466b5d10f4e6ed448f8
Vanessa Sotomayor <vannexxaa@gmail.com>
2021-11-25 05:49:53 +0000

    Create endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace  :v1 do
+      resources :artists, only: [:index] do
+        resources :albums, only: [:index]
+      end
+      resources :albums, only: [:index] do
+        resources :songs, only: [:index]
+      end
+      get '/genres/:genre_name/random_song', to: "songs#random_song"
+    end
+  end
 end
