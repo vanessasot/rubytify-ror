90ec410ec6ec0aa9090028194d99b5160514eea0
Vanessa Sotomayor <vannexxaa@gmail.com>
2021-11-26 03:26:45 +0000

    Configuration endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       resources :artists, only: [:index] do
         resources :albums, only: [:index]
       end
-      resources :albums, only: [:index] do
+      resources :albums do
         resources :songs, only: [:index]
       end
       get '/genres/:genre_name/random_song', to: "songs#random_song"
